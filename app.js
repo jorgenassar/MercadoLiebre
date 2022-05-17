2088ff774ae6ebcc4ac3b68cc04d9301034d7bcd
jorgenassar <104800294+jorgenassar@users.noreply.github.com>
2022-05-16 21:21:24 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.listen(3000, ()=> {
+  console.log('Servidor Corriendo en puerto 3000');
+});
